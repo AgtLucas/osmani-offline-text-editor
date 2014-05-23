b11f3794301338e65476088cc661e7948d0aa2d5
Lucas Churchill <lucas@agtlucas.com>
2014-05-23 13:20:31 -0300

    Updates

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 
   'use strict';
 
-  l = localStorage,
-  k = 'c',
-  q = d.body;
+  var l = localStorage,
+    k = 'c',
+    q = d.body;
 
   q.contentEditable = true;
   q.innerHTML = l[k] || '';
